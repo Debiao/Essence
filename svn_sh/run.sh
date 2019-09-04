5703207fe60ed391db61c2bd631eb89e00b3f16a
Mac <mac@MacdeMac-mini.local>
2019-09-04 17:28:32 +0800

    no message

--- svn_sh/run.sh
+++ svn_sh/run.sh
@@ -12,17 +12,18 @@ LOG_PATH=/Users/mac/Documents/temporary/log
 
 #更新svn本地仓库#
 cd ${SVN_PATH}
-sudo svn update 
-expect << EOF
-set timeout 360
-expect Password: {send "789456\r"}
-EOF
+svn update 
+# sudo svn update 
+# expect << EOF
+# set timeout 360
+# expect Password: {send "789456\r"}
+# EOF
 
-sudo chmod -R 777 ${SVN_PATH}
-expect << EOF
-set timeout 360
-expect Password: {send "789456\r"}
-EOF
+# sudo chmod -R 777 ${SVN_PATH}
+# expect << EOF
+# set timeout 360
+# expect Password: {send "789456\r"}
+# EOF
 
 open ${LOG_PATH}
 
