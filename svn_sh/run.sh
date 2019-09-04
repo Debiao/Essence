1affc15a307899ad985e8202b5a22b714e45298f
Mac <mac@MacdeMac-mini.local>
2019-09-04 14:42:14 +0800

    no message

--- svn_sh/run.sh
+++ svn_sh/run.sh
@@ -8,7 +8,7 @@ set -e
 #本地svn路径
 SVN_PATH=/Users/mac/Documents/workspace/svn/500out/game     # <---------------这里需要修改
 #编译cordova路径
-NPM_RUN_PATH=/Users/mac/Documents/workspace/svn/500out/game/web/WEB_APP    # <---------------这里需要修改
+NPM_RUN_PATH=/Users/mac/Documents/workspace/svn/500out/game/web/WEB_APP_500outgpc_dis    # <---------------这里需要修改
 #<------------------------------------------------> 
    
 #更新svn本地仓库#
@@ -19,7 +19,7 @@ set timeout 3600
 expect "Password:" {send "789456\r"}
 EOF
 cd ${NPM_RUN_PATH}
-npm run ios
+npm run ios-min
 
 
 
