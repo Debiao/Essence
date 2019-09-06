e5a789a4dedab0b94410b66ad4afe2058c160917
Mac <mac@MacdeMac-mini.local>
2019-09-06 17:29:44 +0800

    no message

--- sendemail_sh/balesuccess/run.sh
+++ sendemail_sh/balesuccess/run.sh
@@ -2,8 +2,8 @@
 #script name : sendemail.sh
 VERSION_FILE=/Users/mac/Documents/temporary/log/svn
 # 2200170656@qq.com 丘 sunshuo_1990@126.com 30m_3c4sifuum@dingtalk.com 智冲
-array=(qiuduan6119@dingtalk.com 2677866009@qq.com sunshuo_1990@126.com 613623@qq.com)
-# array=(sunshuo_1990@126.com)
+#array=(qiuduan6119@dingtalk.com 2677866009@qq.com sunshuo_1990@126.com 613623@qq.com)
+array=(sunshuo_1990@126.com)
 account='448966707@qq.com' #发件箱
 password='crnfstwdzeracaeb' #发件箱密码
 SMTP_server='smtp.qq.com' #发件箱对应的stmp服务器
