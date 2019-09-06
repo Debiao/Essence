f3ba85cf1a59094112a3942861c66a0a568ea549
Mac <mac@MacdeMac-mini.local>
2019-09-06 11:29:09 +0800

    no message

---
+++ backupLog_sh/xcode/run.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#set -ex 调试命令
+#报错直接停止
+set -e     
+TIME=$(date "+%Y-%m-%d-%H-%M-%S---")
+ERROR_PATH=/Users/mac/Documents/temporary/log/xcode
+#<------------------------------------------------> 
+
+if [ -f ${ERROR_PATH}/error.log ];then
+        cp ${ERROR_PATH}/error.log ${ERROR_PATH}/${TIME}error.log
+        rm -rf ${ERROR_PATH}/error.log
+        if [ $? -eq 0 ];then
+        echo  "log备份成功"
+        else
+        echo  "log备份失败"
+        fi
+    else
+        echo '!!!' ${ERRORFILE_PATH} '不存在'
+fi 
+
+open ${ERROR_PATH}
---
+++ sendemail_sh/svnupdate/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#script name : sendemail.sh
+VERSION_FILE=/Users/mac/Documents/temporary/log/svn
+# 2200170656@qq.com 丘 sunshuo_1990@126.com
+# array=(qiuduan6119@dingtalk.com 30m_3c4sifuum@dingtalk.com sunshuo_1990@126.com)
+array=(2677866009@qq.com qiuduan6119@dingtalk.com)
+account='448966707@qq.com' #发件箱
+password='crnfstwdzeracaeb' #发件箱密码
+SMTP_server='smtp.qq.com' #发件箱对应的stmp服务器
+
+#收件邮箱
+subject=svn迭代版本 #第二个参数(主题)
+content=$(grep -i '^Updated' ${VERSION_FILE}/*.log) #第三个参数(内容)
+for to in ${array[@]}
+do
+sendemail -f $account -t $to -s $SMTP_server -u $subject -o message-content-type=html -o message-charset=utf-8 -xu $account -xp $password -m $content
+done
