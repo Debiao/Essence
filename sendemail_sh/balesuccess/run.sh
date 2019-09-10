a7cb4e32be9871d669222c82dc2c125a8001425d
Mac <mac@MacdeMac-mini.local>
2019-09-10 10:31:50 +0800

    no message

--- sendemail_sh/balesuccess/run.sh
+++ sendemail_sh/balesuccess/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #script name : sendemail.sh
 XCODE_LOGFILE_PATH=/Users/mac/Documents/temporary/log/xcode
 # 2200170656@qq.com 丘 sunshuo_1990@126.com 30m_3c4sifuum@dingtalk.com 智冲
@@ -8,30 +9,34 @@ account='448966707@qq.com' #发件箱
 password='crnfstwdzeracaeb' #发件箱密码
 SMTP_server='smtp.qq.com' #发件箱对应的stmp服务器
 
-NAME=error
-grep -i '.ipa' /Users/mac/Documents/temporary/log/xcode/error.log
-case $(grep -i '.ipa' ${XCODE_LOGFILE_PATH}/error.log) in
-        "dis_500outgpc.ipa")
-        NAME=股彩(正式服)
-        ;;
-        "dev_500outgpc.ipa")
-        NAME=股彩(测试服)
-        ;;
-esac 
+DIS_NAME=$(grep -i 'dis_500outgpc.ipa' ${XCODE_LOGFILE_PATH}/error.log)
+DEV_NAME=$(grep -i 'dev_500outgpc.ipa' ${XCODE_LOGFILE_PATH}/error.log)
+
+if [ DIS_NAME="dis_500outgpc.ipa" ];then
+    PK_NAME="500outgpc(正式服)"
+fi
+if [ DEV_NAME="dev_500outgpc.ipa" ];then
+    PK_NAME="500outgpc(测试服)"
+fi
+
 #收件邮箱
-if [ $(grep -i 'Push successful.' ${XCODE_LOGFILE_PATH}/error.log) == 'Push successful.' ] then;
-subject=打包 #第二个参数(主题)
-content=$NAME'打包成功!!! 请到这个网址安装https://zt.gzxstech.com/50x.html'
+SUCCESS_NAME=$(grep -i 'Push successful.' ${XCODE_LOGFILE_PATH}/error.log)
+if [ SUCCESS_NAME="Push successful." ];then
+subject=项目打包 #第二个参数(主题)
+contentone=${PK_NAME}
+contenttwo="https://zt.gzxstech.com/50x.html"
+content="项目名字:""<br>""&nbsp;""&nbsp;""&nbsp;""&nbsp;""&nbsp;""&nbsp;""&nbsp;""&nbsp;"${contentone}"<br>""安装地址:""<br>""&nbsp;""&nbsp;""&nbsp;""&nbsp;""&nbsp;""&nbsp;""&nbsp;""&nbsp;"${contenttwo}
 for to in ${array[@]}
 do
 sendemail -f $account -t $to -s $SMTP_server -u $subject -o message-content-type=html -o message-charset=utf-8 -xu $account -xp $password -m $content
 done
 else
-subject=打包 #第二个参数(主题)
-content=$NAME打包失败!!!(请联系管理员---15966817776)
+subject=项目打包 #第二个参数(主题)
+content=${PK_NAME}"打包失败!!!(请联系管理员---15966817776)"
 for to in ${array[@]}
 do
-sendemail -f $account -t $to -s $SMTP_server -u $subject -o message-content-type=html -o message-charset=utf-8 -xu $account -xp $password -m $content
+sendsemail -f $account -t $to -s $SMTP_server -u $subject -o message-content-type=html -o message-charset=utf-8 -xu $account -xp $password -m $content
 done
 fi
+
 
