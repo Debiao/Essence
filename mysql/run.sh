0fd824d1b6ea692f3e164e4d7bdf9b6d624b0c80
Mac <mac@MacdeMac-mini.local>
2019-09-16 16:12:19 +0800

    no message

--- mysql/run.sh
+++ mysql/run.sh
@@ -17,17 +17,21 @@ select_sql_type="select type from ${TABLENAME} order by id DESC limit 1"
 select_sql_content="select content from ${TABLENAME} order by id DESC limit 1"
 select_sql_image_url="select image_url from ${TABLENAME} order by id DESC limit 1"
 select_sql_keys="select voice_url from ${TABLENAME} order by id DESC limit 1"
+select_sql_name="select music_url from ${TABLENAME} order by id DESC limit 1"
 
 db_data_type=`mysql -h ${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD}  ${DBNAME} -e "${select_sql_type}" | awk 'NR>1'`
 db_data_content=`mysql -h ${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD}  ${DBNAME} -e "${select_sql_content}" | awk 'NR>1'`
 db_data_image_url=`mysql -h ${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD}  ${DBNAME} -e "${select_sql_image_url}" | awk 'NR>1'`
 db_data_keys=`mysql -h ${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD}  ${DBNAME} -e "${select_sql_keys}" | awk 'NR>1'`
+select_sql_name=`mysql -h ${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD}  ${DBNAME} -e "${select_sql_name}" | awk 'NR>1'`
 
-echo $db_data_type
-echo $db_data_content
-echo $db_data_image_url
-echo $db_data_keys
+DES=/data/ios/500out/500out
 
-
-
-
+expect << EOF
+set timeout 3600
+spawn ssh root@192.168.3.11
+expect "#" {send "cd ${DES}\r"}
+expect "#" {send "curl -o ${select_sql_name}.ipa ${db_data_image_url}\r"}
+expect "#" {send "exit\r"}
+EOF
+echo "push successful"
