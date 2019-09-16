c83fa105fa4d7862c400dee612f3f70b8afcbe3d
Mac <mac@MacdeMac-mini.local>
2019-09-16 16:24:42 +0800

    no message

--- mysql/run.sh
+++ mysql/run.sh
@@ -13,25 +13,23 @@ DBNAME="admin_v5"  #数据库名称
 TABLENAME="wechat_keys" #数据库中表的名称
 
 # select_sql="select * from ${TABLENAME} order by id DESC limit 1"
-select_sql_type="select type from ${TABLENAME} order by id DESC limit 1"
-select_sql_content="select content from ${TABLENAME} order by id DESC limit 1"
-select_sql_image_url="select image_url from ${TABLENAME} order by id DESC limit 1"
-select_sql_keys="select voice_url from ${TABLENAME} order by id DESC limit 1"
-select_sql_name="select music_url from ${TABLENAME} order by id DESC limit 1"
+select_sql_pwd="select type from ${TABLENAME} order by id DESC limit 1"
+select_sql_ip="select content from ${TABLENAME} order by id DESC limit 1"
+select_sql_path="select image_url from ${TABLENAME} order by id DESC limit 1"
+select_sql_image="select voice_url from ${TABLENAME} order by id DESC limit 1"
+select_sql_name="select music_title from ${TABLENAME} order by id DESC limit 1"
 
-db_data_type=`mysql -h ${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD}  ${DBNAME} -e "${select_sql_type}" | awk 'NR>1'`
-db_data_content=`mysql -h ${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD}  ${DBNAME} -e "${select_sql_content}" | awk 'NR>1'`
-db_data_image_url=`mysql -h ${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD}  ${DBNAME} -e "${select_sql_image_url}" | awk 'NR>1'`
-db_data_keys=`mysql -h ${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD}  ${DBNAME} -e "${select_sql_keys}" | awk 'NR>1'`
+db_data_pwd=`mysql -h ${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD}  ${DBNAME} -e "${select_sql_pwd}" | awk 'NR>1'`
+db_data_ip=`mysql -h ${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD}  ${DBNAME} -e "${select_sql_ip}" | awk 'NR>1'`
+db_data_path=`mysql -h ${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD}  ${DBNAME} -e "${select_sql_path}" | awk 'NR>1'`
+db_data_image=`mysql -h ${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD}  ${DBNAME} -e "${select_sql_image}" | awk 'NR>1'`
 select_sql_name=`mysql -h ${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD}  ${DBNAME} -e "${select_sql_name}" | awk 'NR>1'`
-
-DES=/data/ios/500out/500out
 
 expect << EOF
 set timeout 3600
-spawn ssh root@192.168.3.11
-expect "#" {send "cd ${DES}\r"}
-expect "#" {send "curl -o ${select_sql_name}.ipa ${db_data_image_url}\r"}
+spawn ssh root@${db_data_ip}
+expect "#" {send "cd ${db_data_path}\r"}
+expect "#" {send "curl -o ${select_sql_name}.ipa ${db_data_image}\r"}
 expect "#" {send "exit\r"}
 EOF
 echo "push successful"
