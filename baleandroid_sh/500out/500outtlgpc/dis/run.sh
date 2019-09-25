81063ac680308207814675990fea5bd7f7c6a06a
Mac <mac@MacdeMac-mini.local>
2019-09-25 16:29:39 +0800

    no message

--- baleandroid_sh/500out/500outtlgpc/dis/run.sh
+++ baleandroid_sh/500out/500outtlgpc/dis/run.sh
@@ -4,6 +4,9 @@
 set -e     
 #获取当前时间
 TIME=$(date "+%Y-%m-%d-%H-%M-%S---")
+APK_NAME=app-release.apk
+APN_NEW_NAME=dis_500outtlgpc.apk
+APK_PATH=/Users/mac/Documents/workspace/svn/500out/game/web/WEB_APP_500outtlgpc_dis/platforms/android/app/build/outputs/apk/release
 
 #<------------------------------------------------>                  
 # 上传服务器时用  
@@ -12,7 +15,7 @@ SERVER_PATH=/data/Android/500out/500outtlgpc
 #<------------------------------------------------>  
 
 
-#编译cordova路径
+# #编译cordova路径
 NPM_RUN_PATH=/Users/mac/Documents/workspace/svn/500out/game/web/WEB_APP_500outtlgpc_dis    # <---------------这里需要修改
 cd ${NPM_RUN_PATH}
 npm run android-build
@@ -39,16 +42,38 @@ npm run android-build
 #     fi 
 # fi
 
+echo ${APK_PATH}/${APK_NAME}
+
+if [ -f ${APK_PATH}/${APK_NAME} ];then
+        cd ${APK_PATH}
+        mv ${APK_NAME} ${APN_NEW_NAME}
+        cp  ${APK_PATH}/${APN_NEW_NAME}  ${APK_PATH}/${TIME}${APN_NEW_NAME}
+        rm -rf ${APK_PATH}/${APK_NAME}
+        if [ $? -eq 0 ];then
+        echo ${APN_NEW_NAME} "备份成功"
+        else
+        echo ${APN_NEW_NAME} "备份失败"
+        fi
+    else
+        echo '!!!' ${APK_NAME} '不存在'
+    fi 
 
 
-
-if [ -f ${IPADir_PATH}/${IPA_NAME} ];then
+if [ -f ${APK_PATH}/${APN_NEW_NAME} ];then
     #expect "password:" {send "gzxs311.!@#\r"}
 expect << EOF
-    set timeout 3600
-    spawn scp -r  /Users/mac/Documents/workspace/svn/500out/game/web/WEB_APP_500outtlgpc_dis/platforms/android/app/build/outputs/apk/release/app-release.apk root@zt.gzxstech.com:${SERVER_PATH}
+    set timeout 360
+    spawn scp -r ${APK_PATH}/${APN_NEW_NAME} root@zt.gzxstech.com:${SERVER_PATH}
     expect "#" {send "exit\r"}
 EOF
+# expect << EOF
+#     set timeout 360
+#     spawn 
+#     expect "#" {send "exit\r"}
+# EOF
+
+
+
 
         if [ $? -ne 0 ];then
                 echo "Error,please try again."
