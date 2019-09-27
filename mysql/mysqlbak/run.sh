9bf74e5f096c9492ae81d1836c764477045d6004
Mac <>
2019-09-27 11:02:33 +0800

    20190927-1102

--- mysql/mysqlbak/run.sh
+++ mysql/mysqlbak/run.sh
@@ -5,6 +5,7 @@ DATABASE="admin_v5"
 HOSTNAME="localhost"
 
 BACKUP_DIR=/Users/mac/Documents/workspace/Essence/mysql/mysqlbak/Users/mac/Documents/workspace/Essence/mysql/mysqlbak #备份文件存储路径
+GIT_PATH=/Users/mac/Documents/workspace/Essence
 DATE=`date '+%Y%m%d-%H%M'` #日期格式（作为文件名）
 DUMPFILE=${DATE}.sql #备份文件名
 OPTIONS="-h$HOSTNAME -u$USER -p$PASSWORD --default-character-set=utf8 --skip-lock-tables --databases $DATABASE"
@@ -14,6 +15,7 @@ cd $BACKUP_DIR
 #使用mysqldump 命令备份制定数据库，并以格式化的时间戳命名备份文件
 mysqldump $OPTIONS > $DUMPFILE
 
+cd $GIT_PATH
 git add .
 git commit -m "${DATE}"
 git push
