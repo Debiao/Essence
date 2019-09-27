4b958b5778e756da17a43fbeb5b74f7cea111b59
Mac <mac@MacdeMac-mini.local>
2019-09-27 10:54:50 +0800

    no message

--- mysql/mysqlbak/run.sh
+++ mysql/mysqlbak/run.sh
@@ -4,7 +4,7 @@ PASSWORD="Sun900930"
 DATABASE="admin_v5"   
 HOSTNAME="localhost"
 
-BACKUP_DIR=/Users/mac/Documents/workspace/Essence/mysql/mysqlbak/Users/mac/Documents/workspace/Essence/mysql/mysqlbak/mysqlfile #备份文件存储路径
+BACKUP_DIR=/Users/mac/Documents/workspace/Essence/mysql/mysqlbak/Users/mac/Documents/workspace/Essence/mysql/mysqlbak #备份文件存储路径
 DATE=`date '+%Y%m%d-%H%M'` #日期格式（作为文件名）
 DUMPFILE=${DATE}.sql #备份文件名
 OPTIONS="-h$HOSTNAME -u$USER -p$PASSWORD --default-character-set=utf8 --skip-lock-tables --databases $DATABASE"
@@ -13,6 +13,7 @@ OPTIONS="-h$HOSTNAME -u$USER -p$PASSWORD --default-character-set=utf8 --skip-loc
 cd $BACKUP_DIR
 #使用mysqldump 命令备份制定数据库，并以格式化的时间戳命名备份文件
 mysqldump $OPTIONS > $DUMPFILE
+
 
 
 #清理过期文件
