8f889de633ab9323fa3c03b5c238256078f3ba8b
Mac <mac@MacdeMac-mini.local>
2019-10-25 13:54:31 +0800

    no message

--- mysql/mysqlbak/run.sh
+++ mysql/mysqlbak/run.sh
@@ -4,8 +4,8 @@ PASSWORD="Sun900930"
 DATABASE="admin_v5"   
 HOSTNAME="localhost"
 
-BACKUP_DIR=/Users/mac/Documents/workspace/SQL #备份文件存储路径
-GIT_PATH=/Users/mac/Documents/workspace/SQL
+BACKUP_DIR=/Users/mac/Documents/workspace/Essence/mysql/mysqlbak #备份文件存储路径
+GIT_PATH=/Users/mac/Documents/workspace/Essence
 DATE=`date '+%Y%m%d-%H%M'` #日期格式（作为文件名）
 DUMPFILE=${DATE}.sql #备份文件名
 OPTIONS="-h$HOSTNAME -u$USER -p$PASSWORD --default-character-set=utf8 --skip-lock-tables --databases $DATABASE"
