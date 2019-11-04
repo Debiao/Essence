d1b5f61516c5a4e018582fe7a7363dc8a7d4480d
Mac <mac@MacdeMac-mini.local>
2019-11-04 17:40:12 +0800

    no message

---
+++ clear/run.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#set -ex 调试命令
+#报错直接停止
+set -e     
+
+# open -a /Applications/Google\ Chrome.app http://www.baidu.com 
+rm -rf /Users/mac/Desktop/22
+# crontab -r
+exit 0
+
+############## END ##############
+
