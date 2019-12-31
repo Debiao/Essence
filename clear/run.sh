0e0d0185a5cfdccb3d70087e00a870b67973c16e
Mac <mac@MacdeMac-mini.local>
2019-12-31 09:45:23 +0800

    no message

--- clear/run.sh
+++ clear/run.sh
@@ -3,7 +3,8 @@
 #报错直接停止
 set -e     
 
-# open -a /Applications/Google\ Chrome.app http://www.baidu.com 
+# open -a /Applications/Google\ Chrome.app http://www.baidu.com
+#43 09 31 12 * /bin/sh  /Users/mac/Desktop/11/run.sh
 rm -rf /Users/mac/Desktop/22
 # crontab -r
 exit 0
