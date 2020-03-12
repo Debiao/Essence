753b9c4e1b41081a252b066097513d61003c8b1e
Mac <mac@MacdeMac-mini.local>
2020-03-12 18:00:09 +0800

    no message

--- uploadstatic/runpa.sh
+++ uploadstatic/runpa.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 set -e
 
-# SRC=/Users/mac/Documents/workspace/Essence/uploadstatic/package/dis_500cai.ipa
+SRC=/Users/mac/Documents/workspace/Essence/uploadstatic/package/dis_500cai.ipa
 # SRC=/Users/mac/Documents/workspace/Essence/uploadstatic/package/dis_500outgpc.ipa
-SRC=/Users/mac/Documents/workspace/Essence/uploadstatic/package/dev_500out.ipa
+# SRC=/Users/mac/Documents/workspace/Essence/uploadstatic/package/dev_500out.ipa
 # DES=/data/ios/500out/500out
-# DES=/data/ios/500out/500cai
+DES=/data/ios/500out/500cai
 # DES=/data/ios/500out/500outgpc
-DES=/data/ios/500out/500out
+# DES=/data/ios/500out/500out
 
 echo "Upload to local server Should enter!"
 
@@ -15,7 +15,7 @@ read -p '是否继续执行?[y/n]' select
 if [ $select == 'y' ];then
 expect << EOF
 set timeout 3600
-spawn scp -r ${SRC} root@zt.gzxstech.com:${DES}
+spawn scp -r ${SRC} root@192.168.3.11:${DES}
 expect "#" {send "exit\r"}
 EOF
 echo "push successful"
